1b4238ad623c871d0d6e740ffa9e3adb078b255e
Aswin <aswinkumarak2005@gmail.com>
2026-02-18 21:04:18 +0530

    configure postgres database via environment variables

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,3 +1,16 @@
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'tickets'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': os.getenv('POSTGRES_HOST', 'db'),
+        'PORT': 5432,
+    }
+}
+
 INSTALLED_APPS = [
     ...,
     'rest_framework',
